a009caeadd843f1e92091d47f1ae327875dd2e5a
Xinyuanso <51281832+Xinyuanso@users.noreply.github.com>
2022-07-19 22:22:18 +0800

    Add files via upload

---
+++ BaseNavigation.js
@@ -0,0 +1,26 @@
+function SelfServicebtn()
+{
+        window.location="SelfServiceMain.html";   
+}
+
+function e_votingbtn()
+{ 
+        window.location="E-voting.html";
+}
+
+function campusbtn()
+{ 
+        window.location="CampusLifestyle.html";
+}
+function recordsbtn()
+{ 
+        window.location="login.html";
+}
+function SAPSbtn()
+{ 
+        window.location="login.html";
+}
+function setupbtn()
+{ 
+        window.location="login.html";
+}
---
+++ SelfServiceMain.js
@@ -0,0 +1,40 @@
+function academicbtn()
+{
+        window.location="Academics.html";   
+}
+function campusbtn()
+{
+        window.location="CampusLifestyle.html";   
+}
+function exambtn()
+{
+        window.location="examination.html";   
+}
+function student_infobtn()
+{
+        window.location="StudentInformation.html";   
+}
+function surveybtn()
+{
+        window.location="Survey&Application.html";   
+}
+function student_statusbtn()
+{
+        window.location="StudentStatus.html";   
+}
+function aca_planningbtn()
+{
+        window.location="SelfServiceMain.html";   
+}
+function accountbtn()
+{
+        window.location="AccountActivity.html";   
+}
+function scholarshipbtn()
+{
+        window.location="MMUScholarship.html";   
+}
+function postgraduatebtn()
+{
+        window.location="PosrgraduateService.html";   
+}
